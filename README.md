b9d80bfdbc9cea01f2499fef454c313f4dfa5b25
Gabriel H. <gabriel.dev.x1@protonmail.me>
2026-08-11 12:22:26 -0400

    2026-08-11 Level select (list selection) - Handled core UX, and started working on visuals.

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# 2026-Godot-Project
+# Read me!
 The project I'm starting around August 2026! Be amazed.
+
+## What this project's deal is:
+I wanted to start practicing games development again, and wanted to actually share what I'm doing this time - by jumping straight into using Git via the terminal! I'm redoing this from scratch, but I'm going to see how much I can finalize by the end of the year.
+
+## Current plans:
+I'm starting super simple and will try to actually refine everything here. Here's the current priority queue:
+ - List/Level selection functionality
+ - Scrolling BG menu view (this should be able to move in any direction in 2D space)
+
+...should PROBABLY add a link to this on my portfolio page. At [my super cool neocities website](https://gabriel-dev.neocities.org/). It's super cool and totally not under construction or anything.
+
+## Happy gaming!
+
+(old text)
 
 I had added some very cool text here, twice now, but I was being dumb the first time and overwrote my CTRL+C copy without saving it, and the second time, Xed (Cinnamon's 'Notes' software), crashed because I switched git branches while diagnosing that I needed to install 'lesspipe' packages on my system (and because I was dumb and didn't save).
 
--- project-file/control-list-selection.gd
+++ project-file/control-list-selection.gd
@@ -1,5 +1,14 @@
 extends Control
 
+## What's new?
+# Worked on list movement and cursor UX a bit; UX is solid, and visuals need more work.
+# See _ready() for this session's starting point, and _process_move_cursor_by() for some logic.
+#
+# Also cleaned up a smidge, some code written during workshopping are gonna ✨disappear✨ after this.
+#
+# Also, I think I broke one of the input map keys in Project > Project Settings...;
+# WASD - W doesn't work.
+
 ##TODO:
 # Let's see how much we can bash out in an hour. We did not complete original workset because of cursor_pos confusion. This mildly upsets me.
 # This seems easy, but it's been a WHILE since I did this.
@@ -25,12 +34,18 @@ extends Control
 # - Add a shadowed VBox BG generated during runtime because it's awesome and adds much-needed contrast + depth
 
 # Bruh this is already a total mess, how--? I'll clean this when I clean it.
-var _current_cursor_pos : int = 0
 var _current_scene : String = "res://list_selection.tscn"
+
+var _current_cursor_pos : int = 0
+
+var _label_quantity : int
+
 @export var _VBoxContainer : VBoxContainer # LHand Y- I think??? Y+ is down, Xr.
-var _VBox_move_by_amt : float = 27.0 # diff is comparing the 1st two labels' pos
-var _current_VBox_pos : float = 0.0
+var _default_VBox_pos : float
 var _desired_VBox_pos : float
+var _VBox_move_by_amt : float = 27.0 # diff is comparing the 1st two labels' pos
+#var _current_VBox_pos : float = 0.0 # This is a stupid variable to use practically. Delete after commit.
+
 
 # Easing variables
 var _var_ease_out_amt : float = 0.25
@@ -41,7 +56,19 @@ var _var_ease_out_exp : float = 2.5
 
 
 func _ready() -> void:
-	_desired_VBox_pos = _current_VBox_pos
+	# Prepares VBoxContainer visual movement information
+	_default_VBox_pos = _VBoxContainer.position.y
+	_desired_VBox_pos = _VBoxContainer.position.y
+	# Prepares wrapping for VBox visuals (infinite wrapping, snapping container)
+	# as well as UX cursor movement/selection;
+	##TODO:
+	# v visually snap list
+	# > make before/after stuff
+	# v handle cursor UX
+	_label_quantity = ( # starts counting from int 1
+		_VBoxContainer.get_child_count(false)
+		)
+	print("Label count: ", _label_quantity)
 	pass
 
 func _process(_delta: float) -> void:
@@ -56,9 +83,6 @@ func _process(_delta: float) -> void:
 		pass
 	
 	if Input.is_action_just_pressed("confirm"):
-		# references child script so we know what to load (do cursor control!!)
-		# oof, I don't know HOW this is out of range when trying to do quit
-		# the list had 7 total children, NOT 8 children. Godot's autorename ruined my sub 1hr bc I didn't expect it :(
 		match _VBoxContainer.get_child(_current_cursor_pos).target_load_path:
 			"RELOAD", "reload", "refresh", "":
 				get_tree().change_scene_to_file(_current_scene)
@@ -85,11 +109,10 @@ func _process(_delta: float) -> void:
 	)
 
 func _handle_ease_out ( # I should totally note easing equations sometime (:
-	_ease_amt : float, # I *HOPE* this is how easequad works as recalled, or
-	_ease_exp : float, # else I'm gonna look realll dumb when I push to GitHub.
-	_delta : float # okay this isn't a quad ease, I think. hmm....
+	_ease_amt : float,
+	_ease_exp : float,
+	_delta : float
 	):
-	# dude I was THIS CLOSE to freaking sightreading this equation from memory!!! omg bruv like
 	if _VBoxContainer.position.y != _desired_VBox_pos:
 		# if moving up...
 		if _VBoxContainer.position.y < _desired_VBox_pos:
@@ -97,33 +120,48 @@ func _handle_ease_out ( # I should totally note easing equations sometime (:
 				(_desired_VBox_pos - _VBoxContainer.position.y) # "Where are we going?"
 				* ((1 - _ease_amt) ** _ease_exp) # "What does the ease look like?"
 				)
-			pass
 		# if moving down...
 		else:
 			_VBoxContainer.position.y -= (
 				(_VBoxContainer.position.y - _desired_VBox_pos)
 				* ((1 - _ease_amt) ** _ease_exp)
 				)
-			pass
 	pass
 
-func _process_desired_vbox_pos():
-	pass
-
-func _process_literal_vbox_pos():
-	pass
+# These two funcs are EXTRA stupid, deleting after next commit
+#func _process_desired_vbox_pos():
+#	pass
+#
+#func _process_literal_vbox_pos():
+#	pass
 
 func _process_move_cursor_by(
 	_cursor_move_amt : int # I'm pretty sure this forces coder to pass an int
 	):
 	if _cursor_move_amt == -1:
-		_current_cursor_pos -= 1
-		_desired_VBox_pos += _VBox_move_by_amt
-		pass
+		# If cursor is going up and out of bounds... don't! Cave Johnson would be so proud.
+		if ((_current_cursor_pos + _cursor_move_amt) < 0):
+			print("Cursor wants to be less than 0! Moved to end of list instead.")
+			_current_cursor_pos = (_label_quantity - 1)
+			_VBoxContainer.position.y = _default_VBox_pos - (
+				_VBox_move_by_amt * _label_quantity
+				) - _VBox_move_by_amt
+			_desired_VBox_pos = _default_VBox_pos - (
+				_VBox_move_by_amt * _label_quantity
+				)
+		else:
+			_current_cursor_pos -= 1
+			_desired_VBox_pos += _VBox_move_by_amt
 	elif _cursor_move_amt == +1:
-		_current_cursor_pos += 1
-		_desired_VBox_pos -= _VBox_move_by_amt
-		pass
+		# If we're going down and out of bounds... don't!
+		if ((_current_cursor_pos + _cursor_move_amt) > (_label_quantity - 1)):
+			print("Cursor wants to be more than available! Moved to end of list instead.")
+			_current_cursor_pos = (0)
+			_VBoxContainer.position.y = _default_VBox_pos + _VBox_move_by_amt
+			_desired_VBox_pos = _default_VBox_pos
+		else:
+			_current_cursor_pos += 1
+			_desired_VBox_pos -= _VBox_move_by_amt
 	else:
 		printerr("[control-list-selection.gd]_process_move_cursor_by()
 			catch case: _cursor_move_amt is only supposed to go up/down by one!")
